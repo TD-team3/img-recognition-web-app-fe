7caad671696e4b121ad880f309abb7d739861ac8
ilPas <p4squy@gmail.com>
2020-12-28 21:07:18 +0100

    Porting da css-html

--- webpack.config.js
+++ webpack.config.js
@@ -3,77 +3,78 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'images/[name].[ext]'
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[name].[ext]",
   },
-    devServer: {
-        contentBase: './dist',
-      },
-    module: {
-        rules: [
-            {
-            test: /\.(sa|sc|c)ss$/,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options : {
-                        publicPath:"./"
-                    },
-                },
-                {
-                    loader: "css-loader",
-                },
-                {
-                    loader: "sass-loader",
-                }
-            ],
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "./",
             },
-            {
-              test: /\.html$/i,
-              loader: 'html-loader',
-              options: {
-                attributes: {
-                  list: [
-                    {
-                      tag: 'img',
-                      attribute: 'src',
-                      type: 'src',
-                    },
-                  ]
-                }
-              }
-            },
-          { test: /\.(png|svg|jpg|gif|webm|mp4)$/,
-            use: [
-            {
-              loader: 'file-loader',
-              options: {
-                esModule: false,
-                name: "[name].[ext]",
-                outputPath: "images/",
-                publicPath: "images/",
-              } 
-            }
-            ]
           },
-        ]
-    },
-    plugins: [
-          new HtmlWebpackPlugin({
-            filename: "index.html", 
-            template: path.resolve(__dirname, "src", "index.html")
-          }),
-          /*
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          attributes: {
+            list: [
+              {
+                tag: "img",
+                attribute: "src",
+                type: "src",
+              },
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|gif|webm|mp4)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "[name].[ext]",
+              outputPath: "images/",
+              publicPath: "images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+    /*
           new HtmlWebpackPlugin({
             filename: "drop.html",
             template: path.resolve(__dirname, "src", "drop.html")
           }),
           */
-        new MiniCssExtractPlugin({
-            filename: "style.css"
-          }),
-    ]
-}; 
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+};
