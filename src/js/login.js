c10caebeb44d08abc98c2416c8f80c3ec143190b
alanSechi <alan.sechi@gmail.com>
2020-12-31 14:56:32 +0100

    [fix] fetch method

--- src/js/login.js
+++ src/js/login.js
@@ -10,63 +10,44 @@ submit.addEventListener("click", send);
 
 function send() {
     event.preventDefault()
-        // // console.log(json())
-
-
-    // // function json() {
-    // //     var data = {}
-    // //     data.username = user.value;
-
-
-    // //     data.password = pass.value;
-
-
-    // //     //data = JSON.stringify(data);
-
-    // //     return data;
-    // // }
-
-
 
 
     // post body data 
-    let form = {
-        username: user.value,
+    let form = { username: user.value, password: pass.value };
 
+    let asdf = JSON.stringify(form);
 
-        password: pass.value
-    };
-
-
+    let formData = new FormData();
+    formData.append("data", asdf);
 
     // pass request object to `fetch()`
-    fetch('http://imgrecognitionteam3.pythonanywhere.com/login/', {
-            method: "POST",
-            body: JSON.stringify(form),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-                "Access-Control-Allow-Origin": "*"
-            }
-        })
-        .then(res => res.json())
-        .then(response => {
-            if (response.ok) { console.log("Contenuto ricevuto"); }
+    fetch('https://imgrecognitionteam3.pythonanywhere.com/login/', {
+        method: "POST",
+        body: formData,
+        // headers: {
+        //     "Content-type": "application/json; charset=UTF-8",
 
-            if (response.status >= 100 && response.status < 200) {
-                console.log("Informazioni per il client");
-            }
-            if (response.status >= 300 && response.status < 399) {
-                console.log("Redirezione");
-            }
-            if (response.status >= 400 && response.status < 499) {
-                console.log("Richiesta errata");
-            }
-            if (response.status >= 500 && response.status < 599) {
-                console.log("Errore sul server")
-            }
-        }).catch(err => console.log(err));
+        // }
+    })
 
+    .then(response => {
+        if (response.ok && res.json()) { console.log(res); }
 
+        if (response.status >= 100 && response.status < 200) {
+            console.log("Informazioni per il client");
+        }
+        if (response.status >= 300 && response.status < 399) {
+            console.log("Redirezione");
+        }
+        if (response.status >= 400 && response.status < 499) {
+            console.log("Richiesta errata");
+        }
+        if (response.status >= 500 && response.status < 599) {
+            console.log("Errore sul server")
+        }
+    }).catch(err => console.log(err));
+
+    console.log(asdf)
 
 
 }
