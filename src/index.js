e3f37aaed5931ecf7afc6848e71aff3a0f2e49af
Pasquale Errico <p4squy@gmail.com>
2021-01-02 17:20:27 +0100

    Aggiunto login con SessionStorage (#9)
    * Porting da css-html
    
    * Completata pagina login
    
    * [ADD] script that send the form data to the api
    
    * [FIX] fixing the SEO attributes and adding links
    
    * [ADD] Font import
    
    * [fix] fixing font-family
    
    * [fix] fetch method
    
    * [FIX] Attivato errore modale
    
    * [FIX] SessionStorage
    
    Co-authored-by: alanSechi <alan.sechi@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
-window.Dropzone = require("./js/dropzone");
 import "./styles.scss";
 
 var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
+const user = $("#user");
+const pass = $("#pass");
+const submit = $("#submitButton");
 
-$("#js-result").onclick = function () {
-  $("#js-modalError").classList.remove("c-modal--open");
-  $("#js-modalResult").classList.add("c-modal--open");
-};
+submit.addEventListener("click", send);
 
-$("#js-error").onclick = function () {
-  $("#js-modalResult").classList.remove("c-modal--open");
-  $("#js-modalError").classList.add("c-modal--open");
-};
+function send() {
+  event.preventDefault();
+
+  // post body data
+  let form = { username: user.value, password: pass.value };
+
+  let asdf = JSON.stringify(form);
+
+  let formData = new FormData();
+  formData.append("data", asdf);
+
+  // pass request object to `fetch()`
+  fetch("https://imgrecognitionteam3.pythonanywhere.com/login/", {
+    method: "POST",
+    body: formData,
+    // headers: {
+    //     "Content-type": "application/json; charset=UTF-8",
+
+    // }
+  })
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((data) => {
+          // use data
+          console.log(data["token"]);
+          sessionStorage.setItem("token", data["token"]);
+          sessionStorage.setItem("username", user.value);
+        });
+        window.location.href = "upload-page.html";
+      }
+
+      if (response.status >= 100 && response.status < 200) {
+        console.log("Informazioni per il client");
+      }
+      if (response.status >= 300 && response.status < 399) {
+        console.log("Redirezione");
+      }
+      if (response.status >= 400 && response.status < 499) {
+        $("#js-modalError").classList.add("c-modal--open");
+        console.log("Richiesta errata");
+      }
+      if (response.status >= 500 && response.status < 599) {
+        console.log("Errore sul server");
+      }
+    })
+    .catch((err) => console.log(err));
+
+  console.log(asdf);
+}
