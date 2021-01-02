ae140dbbd2dea36ffe187c11c571b165bfa8404d
JonathanMauroFerrara <69302171+JonathanMauroFerrara@users.noreply.github.com>
2021-01-02 16:32:28 +0100

    [FIXED] dropzone

--- src/index.js
+++ src/index.js
@@ -13,3 +13,33 @@ $("#js-error").onclick = function () {
   $("#js-modalResult").classList.remove("c-modal--open");
   $("#js-modalError").classList.add("c-modal--open");
 };
+
+Dropzone.options.dropzone = {
+  autoProcessQueue: false,
+  addRemoveLinks: true,
+  dictMaxFilesExceeded: "Maximum upload limit reached",
+  dictInvalidFileType: "upload only JPG/PNG/JPEG/GIF/BMP",
+  acceptedFiles: ".png,.jpg,.jpeg,.gif,.bmp",
+  parallelUploads: 10,
+  // uploadMultiple: true,
+  init: function () {
+    var submitButton = document.querySelector("#js-upload");
+    myDropzone = this;
+
+    submitButton.addEventListener("click", function () {
+      myDropzone.processQueue();
+      alert("hello");
+    });
+
+    this.on("complete", function () {
+      if (
+        this.getQueuedFiles().length == 0 &&
+        this.getUploadingFiles().length == 0
+      ) {
+        var _this = this;
+        _this.removeAllFiles();
+      }
+      console.log(this.getUploadingFiles());
+    });
+  },
+};
