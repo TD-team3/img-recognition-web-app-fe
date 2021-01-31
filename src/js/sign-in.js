43c02ee5b024acf4d555235e0b6f77e341941ccb
JonathanMauroFerrara <69302171+JonathanMauroFerrara@users.noreply.github.com>
2021-01-31 13:38:09 +0100

    [FIXED]Sign-in page & script for sign up

--- src/js/sign-in.js
+++ src/js/sign-in.js
@@ -6,6 +6,11 @@ const password_confirm = $("#password_confirm");
 const email = $("#email");
 const name = $("#name");
 const surname = $("#surname");
+const date = $("#date");
+const gender = $("#gender");
+const country = $("#country");
+const submitRegistration = $("#submit_sign_in");
+const fields = document.getElementsByName("input");
 
 function validatePassword() {
   if (password != null) {
@@ -17,59 +22,7 @@ function validatePassword() {
       console.log("qui2");
       password_confirm.setCustomValidity("");
       password_confirm.style.borderBottom = "0.1em solid green";
-
-      const submitRegistration = $("#submit_sign_in");
-      submitRegistration.addEventListener("click", sendRegistration);
-
-      function sendRegistration() {
-        event.preventDefault();
-
-        // post body data
-        let form = {
-          mail: mail.value,
-          name: name.value,
-          surname: surname.value,
-          password: password.value,
-        };
-
-        let asdf = JSON.stringify(form);
-
-        let formData = new FormData();
-        formData.append("data", asdf);
-        // pass request object to `fetch()`
-        fetch("https://imgrecognitionteam3.pythonanywhere.com/signup/", {
-          method: "POST",
-          body: formData,
-          // headers: {
-          //     "Content-type": "application/json; charset=UTF-8",
-
-          // }
-        })
-          .then((response) => {
-            if (response.ok) {
-              $("#js-modalSuccess").classList.add("c-modal--open");
-              setTimeout(function () {
-                window.location.href = "upload-page.html";
-              }, 1400);
-            }
-
-            if (response.status >= 100 && response.status < 200) {
-              console.log("Informazioni per il client");
-            }
-            if (response.status >= 300 && response.status < 399) {
-              console.log("Redirezione");
-            }
-            if (response.status >= 400 && response.status < 499) {
-              console.log("Richiesta errata");
-            }
-            if (response.status >= 500 && response.status < 599) {
-              console.log("Errore sul server");
-            }
-          })
-          .catch((err) => console.log(err));
-
-        console.log(asdf);
-      }
+      verificationFields();
     }
   }
 }
@@ -80,4 +33,76 @@ if (password != null) {
   password_confirm.onkeyup = function () {
     validatePassword();
   };
+}
+
+fields.onchange = function () {
+  verificationFields();
+};
+
+function verificationFields() {
+  if (
+    password != null &&
+    password_confirm != null &&
+    email != null &&
+    name != null &&
+    surname != null &&
+    date != null &&
+    gender != null &&
+    country != null
+  ) {
+  } else {
+    $("#js-emptyError").classList.add("c-modal--open");
+  }
+  // post body data
+}
+
+submitRegistration.onclick = function () {
+  event.preventDefault();
+  sendRegistration();
+};
+
+function sendRegistration() {
+  let form = {
+    mail: mail.value,
+    name: name.value,
+    surname: surname.value,
+    password: password.value,
+  };
+
+  let asdf = JSON.stringify(form);
+
+  let formData = new FormData();
+  formData.append("data", asdf);
+  // pass request object to `fetch()`
+  fetch("https://imgrecognitionteam3.pythonanywhere.com/signup/", {
+    method: "POST",
+    body: formData,
+    // headers: {
+    //     "Content-type": "application/json; charset=UTF-8",
+
+    // }
+  })
+    .then((response) => {
+      if (response.ok) {
+        $("#js-modalRegistrationSuccess").classList.add("c-modal--open");
+        setTimeout(function () {
+          window.location.href = "index.html";
+        }, 2500);
+      }
+
+      if (response.status >= 100 && response.status < 200) {
+        console.log("Informazioni per il client");
+      }
+      if (response.status >= 300 && response.status < 399) {
+        console.log("Redirezione");
+      }
+      if (response.status >= 400 && response.status < 499) {
+        $("#js-registrationError").classList.add("c-modal--open");
+        console.log("Richiesta errata");
+      }
+      if (response.status >= 500 && response.status < 599) {
+        console.log("Errore sul server");
+      }
+    })
+    .catch((err) => console.log(err));
 }
