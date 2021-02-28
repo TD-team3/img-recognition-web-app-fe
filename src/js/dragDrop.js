c3212ca96609aa9d18b173f91aa7bd3e1a250479
ilPas <p4squy@gmail.com>
2021-02-28 11:07:40 +0100

    Removed xhr instead of try/catch

--- src/js/dragDrop.js
+++ src/js/dragDrop.js
@@ -1,107 +1,98 @@
-window.onload(alert("funge"));
-
 var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
 const fileElem = $("#fileElem");
 
 fileElem.onchange = function () {
-  handleFiles(this.files)
+  handleFiles(this.files);
+};
+
+let dropArea = document.getElementById("drop-area");
+let filesDone = 0;
+let filesToDo = 0;
+let progressBar = document.getElementById("progress-bar");
+let uploadProgress = [];
+
+["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, preventDefaults, false);
+});
+
+function preventDefaults(e) {
+  e.preventDefault();
+  e.stopPropagation();
 }
 
-let dropArea = document.getElementById('drop-area')
-let filesDone = 0
-let filesToDo = 0
-let progressBar = document.getElementById('progress-bar')
-let uploadProgress = []
-
-;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, preventDefaults, false)
-})
-
-function preventDefaults (e) {
-  e.preventDefault()
-  e.stopPropagation()
-}
-
-;['dragenter', 'dragover'].forEach(eventName => {
-  dropArea.addEventListener(eventName, highlight, false)
-})
-
-;['dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, unhighlight, false)
-})
+["dragenter", "dragover"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, highlight, false);
+});
+["dragleave", "drop"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, unhighlight, false);
+});
 
 function highlight(e) {
-  dropArea.classList.add('highlight')
+  dropArea.classList.add("highlight");
 }
 
 function unhighlight(e) {
-  dropArea.classList.remove('highlight')
+  dropArea.classList.remove("highlight");
 }
 
-dropArea.addEventListener('drop', handleDrop, false)
+dropArea.addEventListener("drop", handleDrop, false);
 
 function handleDrop(e) {
-  let dt = e.dataTransfer
-  let files = dt.files
+  let dt = e.dataTransfer;
+  let files = dt.files;
 
-  handleFiles(files)
+  handleFiles(files);
 }
 
 function handleFiles(files) {
-  files = [...files]
-  initializeProgress(files.length) // <- Add this line
-  files.forEach(uploadFile)
-  files.forEach(previewFile)
+  files = [...files];
+  initializeProgress(files.length); // <- Add this line
+  files.forEach(uploadFile);
+  files.forEach(previewFile);
 }
 
-function uploadFile(file, i) { // <- Add `i` parameter
-  var url = 'https://imgrecognitionteam3.pythonanywhere.com/upload/'
-  var xhr = new XMLHttpRequest()
-  var formData = new FormData()
-  xhr.open('POST', url, true)
+function uploadFile(file, i) {
+  // <- Add `i` parameter
+  let url = "https://imgrecognitionteam3.pythonanywhere.com/upload_test/";
+  let formData = new FormData();
+  formData.append("file", file);
+  formData.append(
+    "data",
+    '{"username":"' +
+      sessionStorage.getItem("username") +
+      '","token":"' +
+      sessionStorage.getItem("token") +
+      '"}'
+  );
 
-  // Add following event listener
-  xhr.upload.addEventListener("progress", function(e) {
-    updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
+  fetch(url, {
+    method: "POST",
+    body: formData,
   })
-
-  xhr.addEventListener('readystatechange', function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      console.log(xhr)
-      // Done. Inform the user
-    }
-    else if (xhr.readyState == 4 && xhr.status != 200) {
-      // Error. Inform the user
-      console.log(xhr)
-    }
-  })
-
-  formData.append('file', file)
-  xhr.send(formData)
+    .then(progressDone) // <- Add `progressDone` call here
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 function previewFile(file) {
-  let reader = new FileReader()
-  reader.readAsDataURL(file)
-  reader.onloadend = function() {
-    let img = document.createElement('img')
-    img.src = reader.result
-    document.getElementById('gallery').appendChild(img)
-  }
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = function () {
+    let img = document.createElement("img");
+    img.src = reader.result;
+    document.getElementById("gallery").appendChild(img);
+  };
 }
 
-function initializeProgress(numFiles) {
-  progressBar.value = 0
-  uploadProgress = []
-
-  for(let i = numFiles; i > 0; i--) {
-    uploadProgress.push(0)
-  }
+function initializeProgress(numfiles) {
+  progressBar.value = 0;
+  filesDone = 0;
+  filesToDo = numfiles;
 }
 
-function updateProgress(fileNumber, percent) {
-  uploadProgress[fileNumber] = percent
-  let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
-  progressBar.value = total
+function progressDone() {
+  filesDone++;
+  progressBar.value = (filesDone / filesToDo) * 100;
 }
